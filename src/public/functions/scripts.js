4b1e40956c939437f68acb50ed1fd36efb8799dc
JooGo01 <joseakd136@gmail.com>
2021-08-30 17:07:08 -0300

    chat room app

--- src/public/functions/scripts.js
+++ src/public/functions/scripts.js
@@ -85,6 +85,6 @@ function outputUsers(users) {
 document.getElementById('leave-btn').addEventListener('click', () => {
   const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
   if (leaveRoom) {
-    window.location = '../index';
+    window.location = '../';
   }
 });
